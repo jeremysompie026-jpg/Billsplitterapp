5f1f7565df99040562b9119271c5648950d517fa
jeremysompie026-jpg <jeremysompie026@student.unsrat.ac.id>
2025-12-10 23:27:27 +0800

    Update vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Billsplitterapp/", // <--- CHANGE THIS to your actual GitHub repository name
   server: {
     port: 5173,
   },
